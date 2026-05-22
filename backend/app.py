4a5457af7108ccdd0c111a8c11c24f33291bc65b
tomas1881 <tomastordecilla25@gmail.com>
2026-05-21 19:16:51 -0500

    modifique el backend en app.py

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,15 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sympy as sp
 import re
+import os
+
+
 
 app = Flask(__name__)
 CORS(app)
+
+# Puerto para Render
+port = int(os.environ.get("PORT", 5000))
 
 # Convertir sintaxis MATLAB a Python
 def matlab_to_python(expression):
@@ -167,6 +173,11 @@ if __name__ == '__main__':
     print("=" * 60)
     print("🚀 SERVIDOR DE MÉTODOS NUMÉRICOS")
     print("=" * 60)
-    print("🌐 Servidor corriendo en http://localhost:5000")
+    print(f"🌐 Servidor corriendo en puerto {port}")
     print("=" * 60)
-    app.run(host='0.0.0.0', debug=True, port=5000)
+
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False
+    )
